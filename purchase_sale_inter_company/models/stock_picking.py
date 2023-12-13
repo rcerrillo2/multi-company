3d266d2e146d9f1d9b444e51254cc22ff7b8b2b8
rcerrillo2 <rcerrillo@visiotechsecurity.com>
2023-12-13 09:31:22 +0100

    [FIX]purchase_sale_inter_company: cambiado el nombre de la función do_transfer a action_done

--- purchase_sale_inter_company/models/stock_picking.py
+++ purchase_sale_inter_company/models/stock_picking.py
@@ -12,7 +12,7 @@ class StockPicking(models.Model):
     intercompany_picking_id = fields.Many2one(comodel_name='stock.picking')
 
     @api.multi
-    def do_transfer(self):
+    def action_done(self):
         # Only DropShip pickings
         po_picks = self.browse()
         for pick in self.filtered(
@@ -45,4 +45,4 @@ class StockPicking(models.Model):
             po_pick.with_context(
                 force_company=po_pick.company_id.id,
             ).action_done()
-        return super(StockPicking, self).do_transfer()
+        return super(StockPicking, self).action_done()
